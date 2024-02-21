1718715e1769f75db6e8b7c4b4a0d7b0d4c71db8
akorede <emiolasaheed1@gmail.com>
2024-02-21 22:01:59 +0100

    Allow credentials

--- src/app.js
+++ src/app.js
@@ -3,13 +3,17 @@ import cors from 'cors';
 import { config } from 'dotenv';
 import sequelize from './configs/database.js';
 import authRoute from './routes/user.route.js';
+import cookieParser from 'cookie-parser';
 config()
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 app.use(express.json())
-
-app.use(cors())
+app.use(cookieParser());
 
 app.use('/api/v1', authRoute);
 
