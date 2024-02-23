f79d8f6fc9aefb93038a81081344570d75c73a27
akorede <emiolasaheed1@gmail.com>
2024-02-23 18:30:28 +0100

    Set up association between user and message

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import { config } from 'dotenv';
 import sequelize from './configs/database.js';
 import authRoute from './routes/user.route.js';
 import cookieParser from 'cookie-parser';
+import User from './models/user.model.js';
+import Message from './models/message.model.js';
 config()
 
 const app = express();
@@ -21,6 +23,11 @@ app.use((error, req, res, next) => {
   const { message, statusCode} = error;
   res.status(statusCode || 500).send({ message })
 })
+
+User.hasMany(Message, { as: 'sender', foreignKey: 'senderId' })
+Message.belongsTo(User, { foreignKey: 'senderId' })
+User.hasMany(Message, { as: 'receiver', foreignKey: 'receiverId' });
+Message.belongsTo(User, { foreignKey: 'receiverId' });
 
 sequelize.sync({ alter: true })
   .then(() => {
