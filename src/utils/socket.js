ea6e9b8277e1c86f1a3d69d693073077875fa097
akorede <emiolasaheed1@gmail.com>
2024-03-09 18:14:44 +0100

    Update message status to delivered upon user coming online

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { Server } from "socket.io";
 import { createServer } from 'node:http';
+import Message from '../models/message.model.js';
+import { Op } from 'sequelize';
 
 export const app = express();
 export const server = createServer(app);
@@ -14,7 +16,7 @@ export const onlineUsersMap = {};
 
 export const getReceiverSocketId = (receiverId) => onlineUsersMap[receiverId]
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) {
     onlineUsersMap[userId] = socket.id;
@@ -25,5 +27,16 @@ io.on('connection', (socket) => {
     delete onlineUsersMap[userId];
     io.emit('getOnlineUsers', Object.keys(onlineUsersMap))
   })
+  const messages = await Message.findAll({
+    where: {
+      receiverId: userId | null,
+       status: 'sent'
+    },
+  });
+  const messageIds = messages.map((message) => message.id);
+
+  if (messageIds.length > 0) {
+    await Message.update({status: 'delivered'}, { where: { id: messageIds } })
+  } 
 })
 
