2034acd90c277e2f1d3a2ebbbbd5d1a3577619f6
akorede <emiolasaheed1@gmail.com>
2024-04-18 12:45:10 +0100

    Testing socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -21,6 +21,7 @@ io.on('connection', (socket) => {
   socket.on('user', async (user) => {
     const userId = user.id
     onlineUsersMap[userId] = socket.id; 
+    console.log(onlineUsersMap);
     const onlineUsersId = Object.keys(onlineUsersMap).map((id) => +id);
 
     io.emit('getOnlineUsers', onlineUsersId)
